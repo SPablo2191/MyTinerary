dc7fd80dcf9948d3530ee3957d17d4bef8d11ce1
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-05 17:16:39 -0300

    refactor:  navigation bar typed

--- src/components/ui/navBar/NavBarComponent.tsx
+++ src/components/ui/navBar/NavBarComponent.tsx
@@ -1,17 +1,14 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { NavigationItem } from '../../../core/interfaces/ui/navigation.item.interface'
 
-const navigation = [
-  { name: 'Homes', href: '#', current: true },
-  { name: 'Cities', href: '#', current: false },
-]
+
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function NavBar() {
+export default function NavBar({ navigation }: { navigation: NavigationItem[] }) {
   return (
     <Disclosure as="nav">
       {({ open }) => (
@@ -19,6 +16,16 @@ export default function NavBar() {
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
@@ -26,14 +33,14 @@ export default function NavBar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <div className="hidden sm:ml-6 sm:block">
+                <div className="hidden  sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white font-bold' : 'font-bold hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-gray-900 text-white font-bold' : 'font-bold hover:bg-violet-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -43,10 +50,18 @@ export default function NavBar() {
                     ))}
                   </div>
                 </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <button className="bg-violet-800 p-3 ml-3 rounded-lg text-white font-bold">
+                    <span className="inline-flex mx-3">
+                      <UserIcon className="h-6 w-6" aria-hidden="true" />
+                      <p className="ml-2">Login</p>
+                    </span>
+                  </button>
+                </div>
+
               </div>
             </div>
           </div>
-
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
@@ -63,6 +78,12 @@ export default function NavBar() {
                   {item.name}
                 </Disclosure.Button>
               ))}
+              <button className="bg-violet-800 p-3 ml-3 rounded-lg text-white font-bold">
+                <span className="inline-flex mx-3">
+                  <UserIcon className="h-6 w-6" aria-hidden="true" />
+                  <p className="ml-2">Login</p>
+                </span>
+              </button>
             </div>
           </Disclosure.Panel>
         </>
--- src/layouts/mainLayout/MainLayoutComponent.tsx
+++ src/layouts/mainLayout/MainLayoutComponent.tsx
@@ -1,10 +1,15 @@
 import NavBar from "../../components/ui/navBar/NavBarComponent";
+import { NavigationItem } from "../../core/interfaces/ui/navigation.item.interface";
 
 export function MainLayout({children}:any){
+    let navigation : NavigationItem[] = [
+        { name: 'Homes', href: '#', current: true },
+        { name: 'Cities', href: '#', current: false },
+      ]
     return (
         <>
         <div>
-            <NavBar/>
+            <NavBar navigation={navigation} />
         </div>
         <main>{children}</main>
         </>
