365577001472d6bc6b3869d722e93d52e22a37dc
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-05 17:26:02 -0300

    styles: dark mode added

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { MainLayout } from './layouts/mainLayout/MainLayoutComponent'
 function App() {
 
   return (
-    <>
+    <div className=' m-4 text-gray-800 dark:text-white'>
     <MainLayout>
       <p>Hola</p>
     </MainLayout>
-    </>
+    </div>
   )
 }
 
