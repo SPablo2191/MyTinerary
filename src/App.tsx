b401eaa4caecb03e6c9a1c598d2a5d6a29626fc9
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-08 11:34:12 -0300

    feat: add prime react dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Home } from './pages/home/HomeComponent'
-
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 function App() {
 
   return (
+    <PrimeReactProvider>
     <div className=' m-4 text-gray-800 dark:text-white'>
-      <Home/>
+      <Home />
     </div>
+    </PrimeReactProvider>
   )
 }
 
