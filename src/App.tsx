9df6ccab48d7f6e09d0329194f122b72ea9845ab
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-08 12:31:36 -0300

    feat: carousel in process

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Home } from './pages/home/HomeComponent'
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css"; 
 function App() {
 
   return (
