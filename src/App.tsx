53acc87ee0fb06ed8004e6f73236c386ff052214
SPablo2191 <pablosandoval2191@gmail.com>
2023-08-07 21:04:15 -0300

    feat: in process

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
+import { Home } from './pages/home/HomeComponent'
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div className=' m-4 text-gray-800 dark:text-white'>
+      <Home/>
+    </div>
   )
 }
 
